b7ca9af233fb7e261aeb3fba1419707145c1c4c8
yeban207 <lijianqang@163.com>
2018-05-05 12:47:54 +0800

    first commint

---
+++ js/fun.js
@@ -0,0 +1,73 @@
+var appid = '20180130000119675';
+var key = 'FR_PCoJGORi8gmqnXjGh';
+var salt = (new Date).getTime();
+var query;
+// 多个query可以用\n连接  如 query='apple\norange\nbanana\npear'
+var from = 'zh';
+var to = 'en';
+var str1;//= appid + query + salt + key;
+var sign;// = MD5(str1);
+
+var request = null; //异步请求
+
+$('#transte').click(
+    function (event) {
+        query = $('#srctext').val();
+        if (!query) {
+            alert("请输入");
+            // request.abort();
+            return;
+        }
+        // alert(query);
+        str1 = appid + query + salt + key;
+        sign = MD5(str1);
+    }
+)
+
+$('#transte').click(
+    function () {
+        request = $.ajax({
+            url: 'http://api.fanyi.baidu.com/api/trans/vip/translate',
+            type: 'get',
+            dataType: 'jsonp',
+            data: {
+                q: query,
+                appid: appid,
+                salt: salt,
+                from: from,
+                to: to,
+                sign: sign
+            },
+            success: function (data) {
+                var result = data.trans_result;
+                if (result) {
+                    var r = result[0].dst;
+                    $('#srcResulttext').val(r);
+                    console.log(r);
+                }
+                    
+            }
+        });
+    }
+)
+/**
+ * 复制到剪贴板
+ */
+var clipboard = new Clipboard("#copy");
+clipboard.on('success',function(e){
+    alert("复制成功！");
+});
+
+/**
+ * 清除按钮
+ */
+$('#clear1').click(function(){
+	$('#srctext').val('');
+});
+$('#clear2').click(function(){
+	$('#srcResulttext').val('');
+});
+$('#clear3').click(function(){
+	$('#srctext').val('');
+	$('#srcResulttext').val('');
+});
